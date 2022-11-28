1b91e86e66e24b28e53ce6b2648fa4944921615b
SharOFF <sharov-pa@yandex.ru>
2022-11-28 03:34:27 +0300

    Task_013

---
+++ Task_013/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task_013
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 99)
+{
+  while (n > 999)
+  {
+    n = n / 10;
+  }
+Console.WriteLine(n % 10);
+}
+
+else
+Console.WriteLine("третьей цифры нет");
+
