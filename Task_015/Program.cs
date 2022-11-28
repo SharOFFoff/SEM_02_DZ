7b57ba7132b3abd3f44d6d678120dbe2c2e53c23
SharOFF <sharov-pa@yandex.ru>
2022-11-28 03:57:36 +0300

    Task_015

---
+++ Task_015/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task_015
+Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1 || n == 2 || n == 3 || n == 4 || n == 5)
+Console.Write("нет");
+
+else
+Console.Write("да");
